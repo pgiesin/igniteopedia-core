cd2730ea0ddba4dd82158d0e9ed249ede41bd59f
pgiesin <pgiesin@gmail.com>
2022-04-14 10:30:13 -0600

    Fix AccountManager config

--- account_manager/config/prod.exs
+++ account_manager/config/prod.exs
@@ -1,7 +1,6 @@
 import Config
 
-config :account_persistence, AccountPersistence.Runtime.Repo,
+config :repo_manager, RepoManager.Runtime.Repo,
         database: "igniteopedia",
-        ssl: true,
         url: System.get_env("DATABASE_URL")
 
